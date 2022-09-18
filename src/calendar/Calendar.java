5f3b07173188624428645b4fb198b705144ea077
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-09-19 00:41:23 +0900

    요알 계산 알고리즘 추가

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,31 +1,75 @@
 package calendar;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Calendar {
-	private final static int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 1~12월 달의 일수
+	private final static int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 1~12월 달의 일수
+	private final static int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 윤년
 
-	public static int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public static int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month];
+		} else
+			return MAX_DAYS[month];
 	}
 
-	// 월을 입력하면 해당월의 달력을 출력
-	// 일단 1일은 일요일로 설정
+	// 운년구하기
+	public static boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else
+			return false;
+
+	}
+
+	// 년월일의 첫번째 요일구하기
+	private static int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 4; // 1970년 1월 1일은 목요일임
+
+		int count = 0;
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+		for (int i = 1; i <month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		count += day;
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+		return weekday-1;
+	}
 
 	public static void printCalendar(int year, int month) {
 		System.out.println("   " + year + "년" + "   " + month + "월");
 		System.out.println("    일  월  화  수  목  금  토");
 		System.out.println("  ==================");
-		int maxDay = Calendar.getMaxDaysOfMonth(month);
-		for (int i = 1; i < maxDay; i++) {
-			System.out.printf("%3d",i);
-			if(i%7==0) {
+
+		int weekday = getWeekDay(year, month, 1);
+
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
+		int maxDay = Calendar.getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = count < 7 ? count : 0;
+		;
+
+		// print first line
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		// print from second line to last
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim) {
 				System.out.println();
 			}
 		}
-		
-	}
+		System.out.println();
 
+	}
 }
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -4,20 +4,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Prompt  {
+public class Prompt {
+
+	public static int parseDay(String week) {
+		if (week.equals("일"))
+			return 0;
+		else if (week.equals("월"))
+			return 1;
+		else if (week.equals("화"))
+			return 2;
+		else if (week.equals("수"))
+			return 3;
+		else if (week.equals("목"))
+			return 4;
+		else if (week.equals("금"))
+			return 5;
+		else if (week.equals("토"))
+			return 6;
+		return 0;
+
+	}
+
+
+
 	public static void runPrompt() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		while (true) {
+			System.out.println("연도를 입력해주세요.");
+			int year = Integer.parseInt(br.readLine());
 			System.out.println("달을 입력해주세요.(-1을 입력하면 프로그램이 종료됩니다)");
 			int month = Integer.parseInt(br.readLine()); // 달을 입력받음
+			
 			if (month == -1) {
 				break;
 			}
 			if (month > 13) {
 				continue;
 			}
-			Calendar.printCalendar(2022, month);
+			Calendar.printCalendar(year, month);
 		}
 		System.out.println("프로그램이 종료 되었습니다.");
 
